3c33f7f4ea048ff3812ce234214bc4933fbb0e62
Henri de Jong <henridejong@gmail.com>
2016-05-20 19:41:18 +0200

    Added scripts for scenes Lirc file for Pioneer support

---
+++ scripts/allOff.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TV Off
+curl -o /dev/null http://192.168.1.177/?code=E0E019E6&p=3&r=1
+# Pioneer Off
+curl -o /dev/null http://192.168.1.177/?code=A55AD827&p=2&r=5&f=40&d=20
