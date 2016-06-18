1ec5a2301d2b4805d755d6a5a74cc199ffb9d039
Henri de Jong <henri@enri.nl>
2016-06-18 16:16:53 +0200

    Extra scripts

---
+++ scripts/startRadio1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# DAB+ on
+curl -o /dev/null http://192.168.1.177/?code=40BFC837\&p=2\&r=1
+# receiver on
+curl -o /dev/null http://192.168.1.177/?code=A55A58A7\&p=2\&r=5\&f=40\&d=20
+# DAB+ preset 1
+curl -o /dev/null http://192.168.1.177/?code=40BF02FD\&p=2\&r=1
+# receiver A AUX
+curl -o /dev/null http://192.168.1.177/?code=A55AF20D\&p=2\&r=3\&f=40\&d=20
